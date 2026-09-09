5668da1607a7ce8294d167110a181b421e17219c
durgashankard <ddangi8103@gmail.com>
2026-09-09 11:27:07 +0530

    initial Commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+// MiddleWare 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+}
+app.use(cors(corsOptions));
+
+
+app.get("/", (req, res) => {
+    res.send("hello from server");
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server is running at ${PORT} http://localhost:${PORT}`)
+    connectDB()
+});
---
+++ backend/utils/db.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected SuccessFully");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default connectDB; 
